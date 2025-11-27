7aef0a64732bfd7f8191eae7510fc1f7adb096f4
Abhishek-1337 <abhishekrinku4@gmail.com>
2025-11-28 01:12:51 +0530

    http proxy

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import httpPoxy from "http-proxy";
 import http from "http";
 
-const proxy = httpPoxy.createProxy({});
+const proxy = httpPoxy.createProxyServer({});
 
 const server1 = "http://localhost:3000";
 
 const server = http.createServer((req, res) => {
-    proxy.web(req, res, {
-        target: server1,
-        changeOrigin: true
+    console.log(req.pipe);
+
+    const targetUrl = server1 + req.url;
+    const options = {
+        hostname: 'localhost',
+        method: req.method,
+        port: 3000,
+        path: req.url,
+        headers: req.headers
+    };
+
+    const proxyReq = http.request(options, (proxyRes) => {
+        console.log(proxyRes);
+        if(proxyRes.statusCode === undefined) {
+            return;
+        }
+        res.writeHead(proxyRes.statusCode, proxyRes.headers);
+        proxyRes.pipe(res);
     });
+    req.pipe(proxyReq);
 });
 
 server.listen(9000, () => {console.log("Proxy is listening on 9000")});
